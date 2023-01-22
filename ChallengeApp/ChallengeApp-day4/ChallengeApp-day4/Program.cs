2224fe4671e6c83640d10e606cfa6a7e61c9b38d
Mirasek2 <vince51@wp.pl>
2023-01-22 16:09:36 +0100

    Zadanie dzien 4

---
+++ ChallengeApp/ChallengeApp-day4/ChallengeApp-day4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Deklaracja zmiennych
+string name = "Ewa";
+string sex = "Kobieta";
+int age = 29;
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa,lat 33");
+}
+
+else if (sex == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+else if (name == "Mirek" && sex == "mężczyzna" && age == 41)
+{
+    Console.WriteLine("Mirek jest wysokim meżczyzną");
+}
